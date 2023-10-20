e854292b59ea8008d75f055a88604b307827bdba
haydentinker <haydentinker613@gmail.com>
2023-10-20 12:38:32 -0700

    Server setup and vite react app on its way

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,29 @@
-const mongodb= require('mongodb')
-const dotenv=require('dotenv')
-const app = require('./server'); 
-const RestaurantsDAO= require("./dao/restaurantsDAO.js");
-const ReviewsDAO = require('./dao/reviewsDAO');
-dotenv.config()
-const MongoClient=mongodb.MongoClient
-
-const port =process.env.PORT || 8000
-
-MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI,
-    {
-        maxPoolSize:50,
-        wtimeoutMS:25,
-    },
-).catch(err =>{
-    console.error(err.stack)
-    process.exit(1)
-})
-    .then(async client =>{
-        await RestaurantsDAO.injectDB(client)
-        await ReviewsDAO.injectDB(client)
-        app.listen(port,()=>
-        console.log(`listening on port ${port}`))
-    })
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import book_router from "./routes/booksRoute.js";
+import cors from 'cors';
+const app = express();
+app.use(cors())
+// app.use(cors({
+//     origin:'http://localhost:5173',
+//     methods:['GET','POST','PUT','DELETE'],
+//     allowedHeaders:['Content-Type']
+// }));
+app.use(express.json());
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("HELLO");
+});
+app.use('/books',book_router)
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to DB");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect,useState} from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+
+const ShowBook = () => {
+    const [book,setBook]=useState({});
+    const [loading,setLoading]=useState(false);
+    const {id}=useParams();
+
+    useEffect(()=>{
+        setLoading(true);
+        axios
+        .get(`http://localhost:5555/books/${id}`)
+        .then((response) => {
+            setBook(response.data.data);
+
+            setLoading(false);
+        }).catch((error) => {
+            console.log(error);
+            setLoading(false);
+        })
+    },[])
+  return (
+    <div>showBook</div>
+  )
+}
+
+export default ShowBook
